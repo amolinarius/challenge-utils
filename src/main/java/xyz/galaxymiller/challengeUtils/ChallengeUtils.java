eb76da222b372136a058d6cae0a650358b1613bb
amolinarius <89464182+amolinarius@users.noreply.github.com>
2025-03-12 17:54:06 +0100

    Initial Commit

---
+++ src/main/java/xyz/galaxymiller/challengeUtils/ChallengeUtils.java
@@ -0,0 +1,20 @@
+package xyz.galaxymiller.challengeUtils;
+
+import net.minecraft.init.Blocks;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+
+@Mod(modid = ChallengeUtils.MODID, version = ChallengeUtils.VERSION)
+public class ChallengeUtils
+{
+    public static final String MODID = "challenge-utils";
+    public static final String VERSION = "1.0";
+    
+    @EventHandler
+    public void init(FMLInitializationEvent event)
+    {
+		// some example code
+        System.out.println("DIRT BLOCK >> "+Blocks.dirt.getUnlocalizedName());
+    }
+}
